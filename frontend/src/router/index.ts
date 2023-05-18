897655bba85e66d24c19347b3313b3f68fd42b3c
Eyad Ismail <eyadismail.zen@gmail.com>
2023-05-18 18:34:56 +0200

    chore(): deployment

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AddView from '../views/AddView.vue'
-import Notfound from '../views/404.vue'
+import Notfound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
